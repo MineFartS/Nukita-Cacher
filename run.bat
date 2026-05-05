8d71f4a871cb37a19f08620ac9621e6ecaa84a51
Phil Hunt <philiphuntiv2@gmail.com>
2026-05-05 15:11:53 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,11 +3,11 @@
 set "dir=%temp%\nuitka_%Random%"
 
 mkdir "%dir%\%~1"
-xcopy /E /I /Y "%~f1" "%dir%\%~1"
+xcopy /E /I /Y "%~f1" "%dir%\mymod"
 
 (
     echo from runpy import run_module
-    echo if __name__=='__main__': run_module^("%~1", run_name="__main__"^)
+    echo if __name__=='__main__': run_module^("mymod", run_name="__main__"^)
 ) > "%dir%\run.py"
 
 ::python -m pip install "Nuitka[all]"
@@ -15,9 +15,10 @@ xcopy /E /I /Y "%~f1" "%dir%\%~1"
 python -m nuitka ^
     --assume-yes-for-downloads ^
     --onefile ^
-    "--include-module=%~1" ^
+    "--include-module=mymod" ^
     --follow-imports ^
     --remove-output ^
+    --debug ^
     "--output-dir=%dir%" ^
     "%dir%\run.py"
 
