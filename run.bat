c2e81f68caa82cbd20f488a8651787085def7c01
Phil Hunt <philiphuntiv2@gmail.com>
2026-05-05 15:30:01 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,16 +6,21 @@ mkdir "%dir%\%~1"
 xcopy /E /I /Y "%~f1" "%dir%\mymod"
 
 (
-    echo from runpy import run_module
-    echo if __name__=='__main__': run_module^("mymod", run_name="__main__"^)
+    echo import sys, os, runpy
+    echo sys.path.insert^(0, os.path.dirname^(__file__^)^)
+    echo if __name__=='__main__':
+    echo     runpy.run_module^("mymod", run_name="__main__"^)
 ) > "%dir%\run.py"
 
-::python -m pip install "Nuitka[all]"
+
+python -m pip install "Nuitka[all]"
+
 
 python -m nuitka ^
     --assume-yes-for-downloads ^
     --onefile ^
-    "--include-module=mymod" ^
+    --enable-plugin=multiprocessing ^
+    --include-package=multiprocessing ^
     --follow-imports ^
     --remove-output ^
     --debug ^
