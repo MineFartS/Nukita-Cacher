ae6f8c8b59a59fe3b26a5201eab73cc286de60dd
Phil Hunt <philiphuntiv2@gmail.com>
2026-05-05 14:10:00 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,13 +1,23 @@
-@echo off
+@echo on
+
+set "out_dir=%temp%\nukita_%Random%"
 
 python -m pip install "Nuitka[all]"
 
 python -m nuitka ^
     --assume-yes-for-downloads ^
-    --module %1 ^
-    --include-package=%~1 ^
-    --output-dir=%temp%
+    --standalone ^
+    --output-dir="%out_dir%" ^
+    --main="%~1"
 
-cd /d %temp%
+cd /d "%out_dir%\%~1.dist"
 
-python -m %~1
+dir
+
+"%~1.exe"
+
+::python -c "import sys, importlib.util; n='%~n1'; s=importlib.util.find_spec(n); m=importlib.util.module_from_spec(s); m.__dict__.update({'__name__': '__main__', '__file__': s.origin, '__builtins__': __builtins__}); sys.modules['__main__'] = m; s.loader.exec_module(m)"
+
+::python -c "import sys, importlib.util; name='%~n1'; spec=importlib.util.find_spec(name); m=importlib.util.module_from_spec(spec); m.__name__='__main__'; sys.modules[name]=m; spec.loader.exec_module(m)"
+
+:: python -c "import runpy; runpy.run_module('%~1', run_name='__main__', init_globals={})"
